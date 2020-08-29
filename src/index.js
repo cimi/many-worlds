b2ed1a3c33fb8de56f3c9b36edc601a3b6be96ef
Alex Ciminian <alex.ciminian@gmail.com>
2020-08-29 22:33:45 +0100

    Add meyda code to analyze microphone input

--- src/index.js
+++ src/index.js
@@ -1,8 +1,40 @@
 import './index.css';
-
 import * as serviceWorker from './serviceWorker';
+import { getMicMediaStream } from './mic';
+import Meyda from 'meyda';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+async function makeMicAnalyzer() {
+  const mediaStream = await getMicMediaStream();
+  const AudioContext = window.AudioContext // Default
+  || window.webkitAudioContext // Safari and old versions of Chrome
+  || false;
+  const context = new AudioContext();
+  return Meyda.createMeydaAnalyzer({
+    audioContext: context,
+    bufferSize: 512,
+    source: context.createMediaStreamSource(mediaStream),
+    windowingFunction: 'blackman',
+    featureExtractors: ["rms"],
+  });
+};
+
+async function onLoad() {
+  const analyzer = await makeMicAnalyzer();
+
+  function loop(timestamp) {
+    if (analyzer) {
+      const features = analyzer.get(["rms"]);
+      document.body.innerHTML = `<p>${features ? features.rms * 10000 : 0}</p>`;
+    }
+
+    window.requestAnimationFrame(loop);
+  }
+  window.requestAnimationFrame(loop);
+}
+
+window.onload = onLoad;
