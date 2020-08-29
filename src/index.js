998969cc122d124f28fc1fff90004caa931044c4
Alex Ciminian <alex.ciminian@gmail.com>
2020-08-29 23:02:06 +0100

    Another nice glitch setting deJong attractor param to rms

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import './index.css';
-import * as serviceWorker from './serviceWorker';
+import GlslCanvas from 'glslCanvas';
+import Meyda from 'meyda';
+
 import { getMicMediaStream } from './mic';
 import { showFpsCounter } from "./stats";
-import Meyda from 'meyda';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+import { fragmentShader, vertexShader } from './shaders';
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
@@ -24,16 +28,49 @@ async function makeMicAnalyzer() {
   });
 };
 
+const [a, b, c, d] = [-2.5, -2.0, -1.2, 2.0];
+// let a = -2.5;
+// const b = -2.0;
+// const c = -1.2;
+// const d = 2.0;
+
+
+function makeGlslCanvas() {
+  const width = 980;
+  const height = width;
+  const canvas = document.createElement('canvas');
+  canvas.width = width * devicePixelRatio;
+  canvas.height = height * devicePixelRatio;
+  canvas.style = `width: ${width}px; height: ${height}px;border:1px solid black;`;
+
+  const sandbox = new GlslCanvas(canvas);
+  setUniforms(sandbox, a, b, c, d);
+  sandbox.load(fragmentShader, vertexShader);
+  document.getElementById("root").appendChild(canvas);
+  return sandbox;
+}
+
+function setUniforms(sandbox, a, b, c, d) {
+  sandbox.setUniform("u_a", a);
+  sandbox.setUniform("u_b", b);
+  sandbox.setUniform("u_c", c);
+  sandbox.setUniform("u_d", d);
+}
+
 window.onload = async function onLoad() {
   const analyzer = await makeMicAnalyzer();
   showFpsCounter(true);
+  const webGlCanvas = makeGlslCanvas();
+
   requestAnimationFrame(function loop(timestamp) {
+    let rms = 0;
     if (analyzer) {
       const features = analyzer.get(["rms"]);
-      const el = document.getElementById("root");
-      el.innerHTML = `<p>${features ? features.rms * 10000 : 0}</p>`;
+      const el = document.getElementById("meyda-debug");
+      rms = features ? features.rms : 0
+      el.innerHTML = `${rms}`;
     }
-
+    setUniforms(webGlCanvas, Math.sin(timestamp / 8000) + rms, b, c, d);
     requestAnimationFrame(loop);
   });
 };
---
+++ src/shaders.js
@@ -0,0 +1,60 @@
+export const fragmentShader = `
+precision highp float;
+
+varying float v_t;
+
+const float PI = 3.14159265359;
+
+vec3 cubehelix(float x, float y, float z) {
+float a = y * z * (1.0 - z);
+float c = cos(x + PI / 2.0);
+float s = sin(x + PI / 2.0);
+return vec3(
+  z + a * (1.78277 * s - 0.14861 * c),
+  z - a * (0.29227 * c + 0.90649 * s),
+  z + a * (1.97294 * c)
+);
+}
+
+vec3 rainbow(float t) {
+if (t < 0.0 || t > 1.0) t -= floor(t);
+float ts = abs(t - 0.5);
+return cubehelix(
+  (360.0 * t - 100.0) / 180.0 * PI,
+  1.5 - 1.5 * ts,
+  0.8 - 0.9 * ts
+);
+}
+
+void main() {
+gl_FragColor = vec4(rainbow(v_t / 4.0 + 0.25), 1.0);
+}
+`;
+
+export const vertexShader = `
+precision highp float;
+
+const float PI = 3.14159265359;
+
+uniform float u_a;
+uniform float u_b;
+uniform float u_c;
+uniform float u_d;
+
+attribute vec2 a_position;
+
+varying float v_t;
+
+void main() {
+float x1, x2 = a_position.x;
+float y1, y2 = a_position.y;
+for (int i = 0; i < 8; i++) {
+  x1 = x2, y1 = y2;
+  x2 = sin(u_a * y1) - cos(u_b * x1);
+  y2 = sin(u_c * x1) - cos(u_d * y1);
+}
+v_t = atan(a_position.y, a_position.x) / PI;
+gl_Position = vec4(x2 / 2.0, y2 / 2.0, 0.0, 1.0);
+gl_PointSize = 1.5;
+}
+`;
