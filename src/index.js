5ee1bd78a84b3d599562970b0a42de3479da3d33
Alex Ciminian <alex.ciminian@gmail.com>
2020-08-29 22:18:51 +0100

    Remove react dependency and code

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import * as serviceWorker from './serviceWorker';
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
