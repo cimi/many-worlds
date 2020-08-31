550c8d07c2593ef850d67dcbb9bee675f52e125b
Alex Ciminian <alex.ciminian@gmail.com>
2020-08-31 22:04:32 +0100

    Refactor attractor code into class

--- src/index.js
+++ src/index.js
@@ -28,71 +28,96 @@ async function micAnalyzer() {
   });
 };
 
+class GlAttractor {
+  constructor(fragmentShaderCode, vertexShaderCode, numPoints) {
+    this.canvas = document.createElement('canvas');
+    this.setWidth(Math.min(window.innerHeight, window.innerWidth));
+    this.setHeight(this.width);
+    this.setMargin((window.innerHeight - this.height) / 2)
+    this.canvas.style = `width: ${this.width}px; height: ${this.height}px; margin: ${this.margin};`;
+    this.gl = this.canvas.getContext("webgl", {antialias: true, depth: false});
+    this.program = this.compileShaders(fragmentShaderCode, vertexShaderCode);
+    this.gl.useProgram(this.program);
+    this.numPoints = numPoints;
+    this.useVertexBuffer();
 
-const n = Math.pow(2, 20);
+    const rootEl = document.getElementById("root");
+    rootEl.style = `width: ${this.width}px; height: ${this.height}px`;
+    rootEl.appendChild(this.canvas);
+  }
 
-function makeProgram(gl) {
-  const vertexShader = gl.createShader(gl.VERTEX_SHADER);
-  gl.shaderSource(vertexShader, vertexShaderCode);
-  gl.compileShader(vertexShader);
-  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) throw gl.getShaderInfoLog(vertexShader);
+  setWidth(width) {
+    this.width = width;
+    this.canvas.width = width * devicePixelRatio;
+    this.canvas.style = this.canvas.style + `width: ${width}px;`;
+  }
 
-  const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
-  gl.shaderSource(fragmentShader, fragmentShaderCode);
-  gl.compileShader(fragmentShader);
-  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) throw gl.getShaderInfoLog(fragmentShader);
+  setHeight(height) {
+    this.height = height;
+    this.canvas.height = height * devicePixelRatio;
+    this.canvas.style = this.canvas.style + `height: ${height}px;`;
+  }
 
-  const program = gl.createProgram();
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) throw gl.getProgramInfoLog(program);
-  return program;
-}
+  setMargin(margin) {
+    this.margin = margin;
+    this.canvas.style = this.canvas.style + `margin: ${margin} auto;`;
+  }
 
-function glCanvas() {
-  const width = Math.min(window.innerHeight, window.innerWidth);
-  const height = width;
-  const canvas = document.createElement('canvas');
-  canvas.width = width * devicePixelRatio;
-  canvas.height = height * devicePixelRatio;
-  const margin = (window.innerHeight - height) / 2;
-  canvas.style = `width: ${width}px; height: ${height}px; margin: ${margin};`;
+  compileShaders(fragmentShaderCode, vertexShaderCode) {
+    const gl = this.gl;
+    const vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertexShader, vertexShaderCode);
+    gl.compileShader(vertexShader);
+    if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) throw gl.getShaderInfoLog(vertexShader);
 
-  const gl = canvas.getContext("webgl", {antialias: true, depth: false});
-  const program = makeProgram(gl);
-  gl.useProgram(program);
-  const array = new Float32Array(n * 2).map(() => Math.random() * 2 - 1);
-  const vertexBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, array, gl.STATIC_DRAW);
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  const a_position = gl.getAttribLocation(program, "a_position");
-  gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_position);
+    const fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragmentShader, fragmentShaderCode);
+    gl.compileShader(fragmentShader);
+    if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) throw gl.getShaderInfoLog(fragmentShader);
 
-  const rootEl = document.getElementById("root");
-  rootEl.style = `width: ${width}px; height: ${height}px`;
-  rootEl.appendChild(canvas);
-  return { gl, program };
-}
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) throw gl.getProgramInfoLog(program);
 
-function setUniforms(gl, program, a, b, c, d, color) {
-  const u_a = gl.getUniformLocation(program, "u_a")
-  const u_b = gl.getUniformLocation(program, "u_b")
-  const u_c = gl.getUniformLocation(program, "u_c")
-  const u_d = gl.getUniformLocation(program, "u_d")
-  const u_color = gl.getUniformLocation(program, "u_color")
-  gl.uniform1f(u_a, a);
-  gl.uniform1f(u_b, b);
-  gl.uniform1f(u_c, c);
-  gl.uniform1f(u_d, d);
-  gl.uniform1f(u_color, color);
+    return program;
+  }
+
+  useVertexBuffer() {
+    const gl = this.gl;
+    const array = new Float32Array(this.numPoints * 2).map(() => Math.random() * 2 - 1);
+    const vertexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, array, gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
+    const a_position = gl.getAttribLocation(this.program, "a_position");
+    gl.vertexAttribPointer(a_position, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(a_position);
+  }
+
+  setUniforms(a, b, c, d, color) {
+    const u_a = this.gl.getUniformLocation(this.program, "u_a")
+    const u_b = this.gl.getUniformLocation(this.program, "u_b")
+    const u_c = this.gl.getUniformLocation(this.program, "u_c")
+    const u_d = this.gl.getUniformLocation(this.program, "u_d")
+    const u_color = this.gl.getUniformLocation(this.program, "u_color")
+    this.gl.uniform1f(u_a, a);
+    this.gl.uniform1f(u_b, b);
+    this.gl.uniform1f(u_c, c);
+    this.gl.uniform1f(u_d, d);
+    this.gl.uniform1f(u_color, color);
+  }
+
+  draw() {
+    const gl = this.gl;
+    gl.drawArrays(gl.POINTS, 0, this.numPoints);
+  }
 }
 
 window.onload = async function onLoad() {
   showFpsCounter(true);
-  const { gl, program } = glCanvas();
+  const attractor = new GlAttractor(fragmentShaderCode, vertexShaderCode, Math.pow(2, 20));
   let analyzer;
   let energy = 0;
   requestAnimationFrame(function loop(timestamp) {
@@ -103,17 +128,20 @@ window.onload = async function onLoad() {
       energy += features ? features.energy / 2 : 0;
       flatness = features ? features.spectralFlatness : 0;
     }
-    gl.drawArrays(gl.POINTS, 0, n);
     const a = Math.sin(energy + timestamp / 10000) - 2.0;
-    const mode = (Math.floor(energy / 100) % 2) === 0
-    const [b, c] = mode ? [-2.0, -1.2] : [-2.53, -1.61];
+    const [b, c] = ((Math.floor(energy / 100) % 2) === 0) ? [-2.0, -1.2] : [-2.53, -1.61];
     const d = (flatness < .05) ? 2.0 - distort : 2.0;
-    // const color = energy / 100 > 5 ? .8 : Math.max(0.8, (Math.sin(energy) +
-    // 1) / 2);
     const color = Math.min(.8, (Math.sin(timestamp / 8000) + 1) / 2);
-    setUniforms(gl, program, a, b, c, d, color);
 
+    attractor.setUniforms(a, b, c, d, color);
+    attractor.draw();
     requestAnimationFrame(loop);
   });
-  analyzer = await micAnalyzer();
+
+  try {
+    analyzer = await micAnalyzer();
+  } catch (e) {
+    console.log(e);
+  }
+
 };
